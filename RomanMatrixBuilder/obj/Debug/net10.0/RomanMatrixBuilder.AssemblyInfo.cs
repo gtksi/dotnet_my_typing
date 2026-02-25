e106743645fcd41f5efc036d99117188111576d1
goto.kosei <17261815+gtksi@users.noreply.github.com>
2026-02-26 08:08:14 +0900

    fix: add .nojekyll to github pages deployment

--- RomanMatrixBuilder/obj/Debug/net10.0/RomanMatrixBuilder.AssemblyInfo.cs
+++ RomanMatrixBuilder/obj/Debug/net10.0/RomanMatrixBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RomanMatrixBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa4ff2d956d8342fa54c41f723723c2e8e6fce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cf9cbe42166a3847be13c47ee1504ac38991fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RomanMatrixBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RomanMatrixBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
